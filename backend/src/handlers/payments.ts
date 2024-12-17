d8edf0b3ef6bfc57148ac5f18888f30272e966a3
Chiesa <remychiesa14@gmail.com>
2024-12-17 11:37:41 +0200

    approve

--- backend/src/handlers/payments.ts
+++ backend/src/handlers/payments.ts
@@ -54,7 +54,7 @@ export default function mountPaymentsEndpoints(router: Router) {
 
   // approve the current payment
   router.post("/approve", async (req, res) => {
-    console.log(req);
+    console.log(req.session);
     console.log(req.session.currentUser);
     if (!req.session.currentUser) {
       return res.status(401).json({
