2ee4b44dee219d19be35a28144f8138eff5b11cd
Chiesa <remychiesa14@gmail.com>
2024-12-17 13:30:03 +0200

    approve

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -91,8 +91,8 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24,
+      secure: true,
     },
   })
 );
